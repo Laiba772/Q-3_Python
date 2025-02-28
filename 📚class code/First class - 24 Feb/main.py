8db91c0df1cfad805cae93e4f4173780c5e39150
Laiba772 <160595947+Laiba772@users.noreply.github.com>
2025-02-28 19:54:52 +0500

    Add files via upload

---
+++ 📚class code/First class - 24 Feb/main.py
@@ -0,0 +1,8 @@
+
+name: str = "Governer Initiative Artificial Intelligence And Computing"
+print("GIAIC stand for: " , name)
+print("Hello world!")
+print(" GIAIC First class of quater 3")
+
+def giaic():
+     print("How Are You?")
