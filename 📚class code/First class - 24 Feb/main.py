c282e709f3410c877d626d4473e77b939dceea39
Laiba772 <160595947+Laiba772@users.noreply.github.com>
2025-05-25 19:55:02 +0500

    Delete 📚class code directory

--- 📚class code/First class - 24 Feb/main.py
+++
@@ -1,8 +0,0 @@
-
-name: str = "Governer Initiative Artificial Intelligence And Computing"
-print("GIAIC stand for: " , name)
-print("Hello world!")
-print(" GIAIC First class of quater 3")
-
-def giaic():
-     print("How Are You?")
